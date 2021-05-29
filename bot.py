7f48a74d5d3174fa5a0f90123b8bc68f4288b837
sahadathasancme <76065423+sahadathasancme@users.noreply.github.com>
2021-05-29 18:32:59 +0000

    Create bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ SHORTLINKBOT = Client('ShortlinkBot',
 
 @SHORTLINKBOT.on_message(filters.command(['start','help']))
 async def start(_, update):
-    markup = InlineKeyboardMarkup([[InlineKeyboardButton("My Owner 👮", url=f"https://t.me/{OWNER}")]])
+    markup = InlineKeyboardMarkup([[InlineKeyboardButton]])
     await update.reply(
         f"**Hi {update.chat.first_name}!**\n\n"
         "I'm shortlink bot. Just send me link and get adsless short link",
