98bd88579f639d52ac7c1b92687622695146614a
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-07 12:02:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import environ
 
-API_ID = environ.get('API_ID')
+API_ID = int(environ.get('API_ID'))
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
 API_KEY = environ.get('API_KEY')
