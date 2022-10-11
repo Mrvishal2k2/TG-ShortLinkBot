21dd59dfc348bb72904378b6cbd604421c809f19
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2022-10-11 16:01:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,40 @@
 '''
-© All rights reserved by Mrvishal2k2
+Bitly Bot
 
+© All rights reserved by Mrvishal2k2
 Kangers dont f*ckin kang this !!!
 Should have to give credits 😏 else f***off 
 This is only for personal use Dont use this for ur bot channel business 😂
 Thanks to Mahesh Malekar for his Gplinks Bot !!
 '''
 
-# Bitly Bot
 
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-log = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 from pyrogram import Client, filters, idle
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyshorteners import Shortener
 from config import API_ID, API_HASH, BOT_TOKEN, BITLY_KEY, OWNER
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 SHORTLINKBOT = Client('ShortlinkBot',
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             workers=50,
-             sleep_threshold=10)
+                      api_id=API_ID,
+                      api_hash=API_HASH,
+                      bot_token=BOT_TOKEN,
+                      workers=50,
+                      sleep_threshold=10)
 
 
 @SHORTLINKBOT.on_message(filters.command(['start', 'help']))
 async def start(_, update):
     markup = InlineKeyboardMarkup([[InlineKeyboardButton("My Owner 👮", url=f"https://t.me/{OWNER}")]])
     await update.reply(
-        f"**Hi {update.chat.first_name}!**\n\n"
-        "I'm shortlink bot. Just send me link and get adsless short link",
-        reply_markup=markup,
-        quote=True)
+            f"**Hi {update.chat.first_name}!**\n\n"
+            "I'm shortlink bot. Just send me link and get adsless short link",
+            reply_markup=markup,
+            quote=True)
 
 
 @SHORTLINKBOT.on_message(filters.regex(r'https?://[^\s]+'))
@@ -56,14 +55,14 @@ def get_shortlink(url):
     shortened_url = None
     Err = None
     try:
-       if BITLY_KEY:
-           ''' Bittly Shorten'''
-           s = Shortener(api_key=BITLY_KEY)
-           shortened_url = s.bitly.short(url)
-       else:
-           ''' Da.gd : I prefer this '''
-           s = Shortener()
-           shortened_url = s.dagd.short(url)
+        if BITLY_KEY:
+            ''' Bittly Shorten'''
+            s = Shortener(api_key=BITLY_KEY)
+            shortened_url = s.bitly.short(url)
+        else:
+            ''' Da.gd : I prefer this '''
+            s = Shortener()
+            shortened_url = s.dagd.short(url)
     except Exception as error:  # skipcq: PYL-W0703 - sometimes it can give error.
         Err = f"#ERROR: {error}"
         log.info(Err)
