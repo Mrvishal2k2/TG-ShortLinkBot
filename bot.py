55e813d90b82f1eca1c357ac7690f2be0877a543
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2022-10-11 19:37:52 +0530

    Fix deepsource (#12)
    Clean

--- bot.py
+++ bot.py
@@ -1,45 +1,46 @@
-'''
-© All rights reserved by Mrvishal2k2
+"""
+Bitly Bot
 
+© All rights reserved by Mrvishal2k2
 Kangers dont f*ckin kang this !!!
-Should have to give credits 😏 else f***off 
+Should have to give credits 😏 else f***off
 This is only for personal use Dont use this for ur bot channel business 😂
 Thanks to Mahesh Malekar for his Gplinks Bot !!
-'''
+"""
 
-# Bitly Bot
 
 import logging
+from pyrogram import Client, filters, idle
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyshorteners import Shortener
+from config import API_ID, API_HASH, BOT_TOKEN, BITLY_KEY, OWNER
+
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from pyrogram import Client, filters, idle
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyshorteners import Shortener
-from config import *
-
-
 SHORTLINKBOT = Client('ShortlinkBot',
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             workers=50,
-             sleep_threshold=10)
-             
-             
+                      api_id=API_ID,
+                      api_hash=API_HASH,
+                      bot_token=BOT_TOKEN,
+                      workers=50,
+                      sleep_threshold=10)
 
-@SHORTLINKBOT.on_message(filters.command(['start','help']))
+
+@SHORTLINKBOT.on_message(filters.command(['start', 'help']))
 async def start(_, update):
+    """Start Message"""
     markup = InlineKeyboardMarkup([[InlineKeyboardButton("My Owner 👮", url=f"https://t.me/{OWNER}")]])
     await update.reply(
-        f"**Hi {update.chat.first_name}!**\n\n"
-        "I'm shortlink bot. Just send me link and get adsless short link",
-        reply_markup=markup,
-        quote=True)
+            f"**Hi {update.chat.first_name}!**\n\n"
+            "I'm shortlink bot. Just send me link and get adsless short link",
+            reply_markup=markup,
+            quote=True)
+
 
 @SHORTLINKBOT.on_message(filters.regex(r'https?://[^\s]+'))
 async def link_handler(_, update):
+    """Link Handler"""
     link = update.matches[0].group(0)
     shortened_url, Err = get_shortlink(link)
     if shortened_url is None:
@@ -50,25 +51,27 @@ async def link_handler(_, update):
     markup = InlineKeyboardMarkup([[InlineKeyboardButton("Link 🔗", url=shortened_url)]])
     # i don't think this bot with get sending message error so no need of exceptions
     await update.reply_text(text=message, reply_markup=markup, quote=True)
-      
+
+
 def get_shortlink(url):
+    """Get Shortlink"""
     shortened_url = None
     Err = None
     try:
-       if BITLY_KEY:
-           ''' Bittly Shorten'''
-           s = Shortener(api_key=BITLY_KEY)
-           shortened_url = s.bitly.short(url)
-       else:
-           ''' Da.gd : I prefer this '''
-           s = Shortener()
-           shortened_url = s.dagd.short(url)
-    except Exception as error:
+        if BITLY_KEY:
+            # Bittly Shorten
+            s = Shortener(api_key=BITLY_KEY)
+            shortened_url = s.bitly.short(url)
+        else:
+            # Da.gd : I prefer this
+            s = Shortener()
+            shortened_url = s.dagd.short(url)
+    except Exception as error:  # skipcq: PYL-W0703 - sometimes it can give error.
         Err = f"#ERROR: {error}"
         log.info(Err)
-    return shortened_url,Err
-        
-        
+    return shortened_url, Err
+
+
 if __name__ == "__main__" :
     SHORTLINKBOT.start()
     log.info(">>Bot-Started<<")
